247855416851086148a3f214530b59bfd21026ae
loroxxx <loroxxx@126.com>
2018-05-06 22:33:50 +0800

    fix

---
+++ src/main/java/com/luo/DynamicContext.java
@@ -0,0 +1,67 @@
+package com.luo;
+
+import com.luo.bean.ClassInfo;
+import com.luo.bean.Scanner;
+import com.luo.kit.StringKit;
+import com.luo.reader.ClassPathClassReader;
+import com.luo.reader.JarReaderImpl;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import java.net.URL;
+import java.util.Enumeration;
+import java.util.Set;
+import java.util.stream.Stream;
+
+@Slf4j
+@NoArgsConstructor
+public final class DynamicContext {
+
+    private static final ClassReader classpathReader = new ClassPathClassReader();
+    private static final ClassReader jarReader       = new JarReaderImpl();
+    private static final String      SUFFIX_JAR      = ".jar";
+
+    private static boolean isJarContext = false;
+
+    public static void init(Class<?> clazz) {
+        String rs = clazz.getResource("").toString();
+        if (rs.contains(SUFFIX_JAR)) {
+            isJarContext = true;
+        }
+    }
+
+    public static Stream<ClassInfo> recursionFindClasses(String packageName) {
+        Scanner scanner    = Scanner.builder().packageName(packageName).recursive(true).build();
+        Set<ClassInfo> classInfos = getClassReader(packageName).readClasses(scanner);
+        return classInfos.stream();
+    }
+
+    public static ClassReader getClassReader(String packageName) {
+        if (isJarPackage(packageName)) {
+            return jarReader;
+        }
+        return classpathReader;
+    }
+
+    public static boolean isJarPackage(String packageName) {
+        if (StringKit.isBlank(packageName)) {
+            return false;
+        }
+        try {
+            packageName = packageName.replace(".", "/");
+            Enumeration<URL> dirs = DynamicContext.class.getClassLoader().getResources(packageName);
+            if (dirs.hasMoreElements()) {
+                String url = dirs.nextElement().toString();
+                return url.indexOf(".jar!") != -1 || url.indexOf(".zip!") != -1;
+            }
+        } catch (Exception e) {
+            log.error("", e);
+        }
+        return false;
+    }
+
+    public static boolean isJarContext() {
+        return isJarContext;
+    }
+
+}
